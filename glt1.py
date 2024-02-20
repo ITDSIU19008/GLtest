b17bdb3a7065e4a892c075d19ae8bb497d83375f
linhvu <linhvu8998@gmail.com>
2024-02-20 10:41:04 +0700

    catdraw

---
+++ glt1.py
@@ -0,0 +1,15 @@
+def print_cat():
+    cat = [
+        "  /\\_/\\",
+        " / o o \\",
+        "(   \"   )",
+        " \\~(*)~/",
+        "  /   \\",
+        " /_____\\"
+    ]
+    for line in cat:
+        print(line)
+
+if __name__ == "__main__":
+    print_cat()
+
